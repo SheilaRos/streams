01f5bb54f317292905bced532d0c4ceb30af0a8e
Yuna114 <aheila.ros.cheme@gmail.com>
2016-12-20 02:55:31 +0100

    faltan 2

--- src/streams/BaloncestoAPP.java
+++ src/streams/BaloncestoAPP.java
@@ -1,10 +1,10 @@
 package streams;
 
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 /**
  * Created by dam on 14/12/16.
@@ -14,6 +14,10 @@ public class BaloncestoAPP {
         Equipo e1 = new Equipo("Los Michael", "Barcelona", LocalDate.of(2015, 10, 15));
         Equipo e2 = new Equipo("Nananana", "Madrid", LocalDate.of(2014, 1, 5));
         Equipo e3 = new Equipo("My Little Nightmare", "Valencia", LocalDate.of(2015, 11, 1));
+        List<Equipo> equipos = new ArrayList<>();
+        equipos.add(e1);
+        equipos.add(e2);
+        equipos.add(e3);
         List<Jugador> jugadores = Arrays.asList(new Jugador("Pocholo", LocalDate.of(1992, 5, 16), (int) (Math.random() * 8000), e1),
                 new Jugador("Fulgencio", LocalDate.of(1993, 7, 1), (int) (Math.random() * 300)+200, e1),
                 new Jugador("Amancio", LocalDate.of(1992, 11, 6), (int) (Math.random() * 8000), e1),
@@ -24,13 +28,15 @@ public class BaloncestoAPP {
                 new Jugador("Briselda", LocalDate.of(1991, 3, 1), (int) (Math.random()* 300)+200, e3),
                 new Jugador("Ricarda", LocalDate.of(1994, 9, 10), (int) (Math.random() * 8000), e3)
         );
-        System.out.println(mas500Canastas(jugadores));
-        System.out.println(entre20y500Canastas(jugadores));
+       // System.out.println(mas500Canastas(jugadores));
+       /* System.out.println(entre20y500Canastas(jugadores));
         System.out.println(nombreJugadoresentre20y500Canastas(jugadores));
         System.out.println(jugadoresOrdenadosNacimiento(jugadores));
         System.out.println(jugadoresOrdenadosCanastas(jugadores));
         System.out.println(jugadores5MasCanastas(jugadores));
-        System.out.println(entre20y500CanastasOrdenadosNacimiento(jugadores));
+        System.out.println(entre20y500CanastasOrdenadosNacimiento(jugadores));*/
+       // System.out.println(jugadorMas4000Canastas(jugadores));
+        System.out.println(jugadorEntre200y500CanastasOrdenadosCanastasONacimiento(jugadores));
 
     }
 
@@ -58,6 +64,41 @@ public class BaloncestoAPP {
     }
 
     public static Jugador jugadorMinCanastas(List<Jugador> jugadores){
+        return jugadores.stream().min(Comparator.comparing(Jugador::getCanastas)).get();
+    }
+    public static Jugador jugadorMaxCanastas(List<Jugador> jugadores){
+        return jugadores.stream().max(Comparator.comparing(Jugador::getCanastas)).get();
+    }
+    public static double avgCanastas(List<Jugador> jugadores){
+        return jugadores.stream().mapToDouble(Jugador::getCanastas).average().getAsDouble();
+    }
+    public static List<Jugador> jugadoresEquipoBCN(List<Jugador> jugadores){
+        return jugadores.stream().filter(jugador -> jugador.getEquipo().getLocalidad().equalsIgnoreCase("Barcelona")).collect(Collectors.toList());
+    }
+    public static boolean jugadorMas4000Canastas(List<Jugador> jugadores){
+        return jugadores.stream().anyMatch(jugador -> jugador.getCanastas()>4000);
+    }
+    public static boolean jugadoresMas50(List<Jugador> jugadores){
+        return jugadores.stream().allMatch(jugador -> jugador.getCanastas()>50);
+    }
+
+    public static boolean jugadorMas50Canastas1Equipo(List<Jugador> jugadores){
+        return jugadores.stream().allMatch(jugador ->  jugador.getCanastas()>50 && jugador.getEquipo().getNombreEquipo().equalsIgnoreCase("Los Michael"));
+    }
+
+    public static List<Jugador> jugadorEntre200y500CanastasOrdenadosCanastasONacimiento(List<Jugador> jugadores){
+        return jugadores.parallelStream()
+                .filter(jugador -> jugador.getCanastas()<=500 && jugador.getCanastas()>=200)
+                .parallel().sorted(Comparator.comparing(Jugador::getCanastas).thenComparing(Jugador::getNacimiento))
+                .collect(Collectors.toList());
+    }
+    public static Map<String,List<Equipo>> equiposAgrupadosPorLocalidad(List<Equipo> equipos){
+        return equipos.stream().collect(groupingBy(Equipo::getLocalidad));
+    }
+    public static Map<String, Integer> sumaCanstasPorEquipo(List<Jugador> jugadores){
+        return ;
+    }
+    public static Integer sumaCanstas(List<Jugador> jugadores){
         return ;
     }
 
